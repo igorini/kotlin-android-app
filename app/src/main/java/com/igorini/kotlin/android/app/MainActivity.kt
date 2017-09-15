57ae384705a8496577007bd0c45bc0cd4a585354
Igor Maguskin <maguskin.igor@gmail.com>
2017-09-15 23:36:29 +0100

    Add space to KDoc in MainActivity.kt

--- app/src/main/java/com/igorini/kotlin/android/app/MainActivity.kt
+++ app/src/main/java/com/igorini/kotlin/android/app/MainActivity.kt
@@ -3,7 +3,7 @@ package com.igorini.kotlin.android.app
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
-/** Represents a main activity*/
+/** Represents a main activity */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
