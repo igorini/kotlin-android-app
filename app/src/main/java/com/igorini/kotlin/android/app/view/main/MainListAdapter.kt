9ead8a0e861f20dc308afe85c8450d9b07214a6d
Igor Maguskin <maguskin.igor@gmail.com>
2017-09-21 22:21:53 +0100

    Add MainListAdapter.kt

---
+++ app/src/main/java/com/igorini/kotlin/android/app/view/main/MainListAdapter.kt
@@ -0,0 +1,7 @@
+package com.igorini.kotlin.android.app.view.main
+
+import com.igorini.kotlin.android.app.view.common.AnyItemAdapter
+import com.igorini.kotlin.android.app.view.common.RecyclerListAdapter
+
+/** Represents an adaapter for a main list  */
+class MainListAdapter(items: List<AnyItemAdapter>) : RecyclerListAdapter(items)
