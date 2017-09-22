d98681df976e4f5ee8b609843a847644fe1c6318
Igor Maguskin <maguskin.igor@gmail.com>
2017-09-22 18:26:46 +0100

    Add CatItemAdapter.kt

---
+++ app/src/main/java/com/igorini/kotlin/android/app/view/main/CatItemAdapter.kt
@@ -0,0 +1,27 @@
+package com.igorini.kotlin.android.app.view.main
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import com.igorini.kotlin.android.app.R
+import com.igorini.kotlin.android.app.model.Cat
+import com.igorini.kotlin.android.app.view.common.ItemAdapter
+
+/** Represents an item adapter for a cat icon */
+class CatItemAdapter(
+        val character: Cat
+) : ItemAdapter<CatItemAdapter.ViewHolder>(R.layout.icon) {
+
+    override fun onCreateViewHolder(itemView: View) = ViewHolder(itemView)
+
+    override fun ViewHolder.onBindViewHolder() {
+        textView.text = character.name
+        imageView.loadImage(character.imageUrl)
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val textView by bindView<TextView>(R.id.textView)
+        val imageView by bindView<ImageView>(R.id.imageView)
+    }
+}
