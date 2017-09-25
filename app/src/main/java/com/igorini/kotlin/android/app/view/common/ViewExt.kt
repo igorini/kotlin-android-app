0fa16afbf00f6681c539fac1c1c96d3bcf8abddc
Igor Maguskin <maguskin.igor@gmail.com>
2017-09-26 00:12:22 +0100

    Add bindView() extension function to Activity in ViewExt.kt

--- app/src/main/java/com/igorini/kotlin/android/app/view/common/ViewExt.kt
+++ app/src/main/java/com/igorini/kotlin/android/app/view/common/ViewExt.kt
@@ -1,6 +1,8 @@
 package com.igorini.kotlin.android.app.view.common
 
+import android.app.Activity
 import android.support.v7.widget.RecyclerView
 import android.view.View
 
+fun <T : View> Activity.bindView(viewId: Int) = lazy { findViewById<T>(viewId) }
 fun <T : View> RecyclerView.ViewHolder.bindView(viewId: Int) = lazy { itemView.findViewById<T>(viewId) }
