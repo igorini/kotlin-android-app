804dd9408478e5f8de878ebab3271c62d7049e6e
Igor Maguskin <maguskin.igor@gmail.com>
2017-09-26 01:00:11 +0100

    Add `loadImage()` extension function to ImageView in ViewExt.kt

--- app/src/main/java/com/igorini/kotlin/android/app/view/common/ViewExt.kt
+++ app/src/main/java/com/igorini/kotlin/android/app/view/common/ViewExt.kt
@@ -3,6 +3,11 @@ package com.igorini.kotlin.android.app.view.common
 import android.app.Activity
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 
 fun <T : View> Activity.bindView(viewId: Int) = lazy { findViewById<T>(viewId) }
+
 fun <T : View> RecyclerView.ViewHolder.bindView(viewId: Int) = lazy { itemView.findViewById<T>(viewId) }
+
+fun ImageView.loadImage(photoUrl: String) = Glide.with(context).load(photoUrl).into(this)
