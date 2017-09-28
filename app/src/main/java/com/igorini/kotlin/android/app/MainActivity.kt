38c7d05f8e711af1bda1416aca0fbb00bd63f4ae
Igor Maguskin <maguskin.igor@gmail.com>
2017-09-28 03:32:14 +0100

    Display main activity without a title

--- app/src/main/java/com/igorini/kotlin/android/app/MainActivity.kt
+++ app/src/main/java/com/igorini/kotlin/android/app/MainActivity.kt
@@ -2,12 +2,14 @@ package com.igorini.kotlin.android.app
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Window
 
 /** Represents a main activity */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_main)
     }
 }
