34efec9ca4539e55c18d3c890c1fa22fcbd6274e
Igor Maguskin <maguskin.igor@gmail.com>
2017-10-17 23:36:42 +0100

    Add MainActivityUi

--- app/src/main/java/com/igorini/kotlin/android/app/MainActivity.kt
+++ app/src/main/java/com/igorini/kotlin/android/app/MainActivity.kt
@@ -3,6 +3,9 @@ package com.igorini.kotlin.android.app
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.Window
+import android.widget.Button
+import android.widget.EditText
+import org.jetbrains.anko.*
 
 /** Represents a main activity */
 class MainActivity : AppCompatActivity() {
@@ -11,5 +14,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_main)
+    }
+}
+
+/** Represents a main activity UI */
+class MainActivityUi : AnkoComponent<MainActivity> {
+    private val customStyle = { v: Any ->
+        when (v) {
+            is Button -> v.textSize = 26f
+            is EditText -> v.textSize = 24f
+        }
+    }
+
+    override fun createView(ui: AnkoContext<MainActivity>) = with(ui) {
+        verticalLayout {
+            padding = dip(32)
+        }.applyRecursively(customStyle)
     }
 }
